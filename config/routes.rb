5b00a675f35866adc2b1305c63d5c50da6d62cd6
Huarn <juanpablo.arevalo@cumplo.com>
2019-04-11 23:41:24 -0400

    feat(routes): Added new update route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'cities/find_city_by_zip_code' => 'cities#find_city_by_zip_code', as: :find_city_zip_api
 
   get 'home/update', to: 'home#update'
+  get 'home/fetch_wind_conditions', to: 'home#fetch_wind_conditions'
 
   root 'home#index'
 end
